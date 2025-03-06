ebd55408f93a6133d87d031e6e7e5cb55dd5fdfe
achau11 <achau1019@gmail.com>
2025-03-06 15:24:23 -0500

    added dynamic data

---
+++ JRNL/Journal List Screen/Model/JournalEntry.swift
@@ -0,0 +1,8 @@
+//
+//  JournalEntry.swift
+//  JRNL
+//
+//  Created by Ahmed on 2025-03-06.
+//
+
+import Foundation
---
+++ JRNL/Journal List Screen/View/JournalListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  JournalListTableViewCell.swift
+//  JRNL
+//
+//  Created by Ahmed on 2025-03-06.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
